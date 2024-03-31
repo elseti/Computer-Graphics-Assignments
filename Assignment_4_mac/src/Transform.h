6bf1f79d79c4679d820b3e89c3575e5ffdc09640
Eliana Setiabudi <amidis100@gmail.com>
2024-03-31 15:29:28 +0800

    implemented transform

--- Assignment_4_mac/src/Transform.h
+++ Assignment_4_mac/src/Transform.h
@@ -22,7 +22,32 @@ public:
     /// TODO: implement this function so that the intersect function first transforms the ray into the object's local coordindate frame
     virtual bool intersect( const Ray& r , Hit& h , float tmin)
     {
-        return false;   // remove this sentence in your implementation
+      // create inverse of matrix
+      mat4 matInv = inverse(m);
+
+      // transform ray origin
+      vec4 oVec4 = vec4(r.getOrigin(), 1.0f);
+      vec3 oTrans = vec3(matInv * oVec4);
+
+      // transform ray direction
+      vec4 dVec4 = vec4(r.getDirection(), 0.0f);
+      vec3 dTrans = vec3(matInv * dVec4);
+
+      // make new ray with the transformed
+      Ray transRay = Ray(oTrans, dTrans);
+
+      // transform normal
+      vec4 n = vec4(h.getNormal(), 0.0f);
+      vec3 nTrans = normalize(vec3(transpose(matInv) * n));
+
+      // delegate to intersect function of object
+      if (o->intersect(transRay, h, tmin)) {
+        h.set(h.getT(), h.getMaterial(), nTrans);
+        return true;
+      }
+
+      return false;
+  
     }
 
  protected:
